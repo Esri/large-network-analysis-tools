a2bf1295150a002def29cee30031f43112d661cc
Melinda Morang <mmorang@esri.com>
2022-01-28 13:51:36 -0800

    Update tests for different output types

--- unittests/test_tool.py
+++ unittests/test_tool.py
@@ -17,6 +17,7 @@ Copyright 2022 Esri
 import sys
 import os
 import datetime
+from glob import glob
 import unittest
 import arcpy
 import portal_credentials
@@ -50,14 +51,14 @@ class TestSolveLargeODCostMatrixTool(unittest.TestCase):
         arcpy.SignInToPortal(self.portal_nd, portal_credentials.PORTAL_USERNAME, portal_credentials.PORTAL_PASSWORD)
 
         # Create a unique output directory and gdb for this test
-        self.output_folder = os.path.join(
+        self.scratch_folder = os.path.join(
             CWD, "TestOutput", "Output_Tool_" + datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
-        os.makedirs(self.output_folder)
-        self.output_gdb = os.path.join(self.output_folder, "outputs.gdb")
+        os.makedirs(self.scratch_folder)
+        self.output_gdb = os.path.join(self.scratch_folder, "outputs.gdb")
         arcpy.management.CreateFileGDB(os.path.dirname(self.output_gdb), os.path.basename(self.output_gdb))
 
-    def test_run_tool_time_units(self):
-        """Test that the tool runs with a time-based travel mode."""
+    def test_run_tool_time_units_feature_class(self):
+        """Test that the tool runs with a time-based travel mode. Write output to a feature class."""
         # Run tool
         out_od_lines = os.path.join(self.output_gdb, "Time_ODLines")
         out_origins = os.path.join(self.output_gdb, "Time_Origins")
@@ -65,15 +66,17 @@ class TestSolveLargeODCostMatrixTool(unittest.TestCase):
         arcpy.LargeNetworkAnalysisTools.SolveLargeODCostMatrix(  # pylint: disable=no-member
             self.origins,
             self.destinations,
-            out_od_lines,
-            out_origins,
-            out_destinations,
             self.local_nd,
             self.local_tm_time,
             "Minutes",
             "Miles",
             50,  # chunk size
             4,  # max processes
+            out_origins,
+            out_destinations,
+            "Feature class",
+            out_od_lines,
+            None,
             15,  # cutoff
             1,  # number of destinations
             None,  # barriers
@@ -84,26 +87,91 @@ class TestSolveLargeODCostMatrixTool(unittest.TestCase):
         self.assertTrue(arcpy.Exists(out_origins))
         self.assertTrue(arcpy.Exists(out_destinations))
 
-    def test_run_tool_distance_units(self):
-        """Test that the tool runs with a distance-based travel mode. Also use barriers."""
+    def test_run_tool_distance_units_csv_barriers(self):
+        """Test that the tool runs with a distance-based travel mode. Write output to CSVs. Also use barriers."""
         # Run tool
-        out_od_lines = os.path.join(self.output_gdb, "Dist_ODLines")
         out_origins = os.path.join(self.output_gdb, "Dist_Origins")
         out_destinations = os.path.join(self.output_gdb, "Dist_Destinations")
+        out_folder = os.path.join(self.scratch_folder, "DistUnits_CSVs")
         arcpy.LargeNetworkAnalysisTools.SolveLargeODCostMatrix(  # pylint: disable=no-member
             self.origins,
             self.destinations,
-            out_od_lines,
-            out_origins,
-            out_destinations,
             self.local_nd,
             self.local_tm_dist,
             "Minutes",
             "Miles",
             50,  # chunk size
             4,  # max processes
+            out_origins,
+            out_destinations,
+            "CSV files",
+            None,
+            out_folder,
             5,  # cutoff
             2,  # number of destinations
+            None,  # barriers
+            True  # precalculate network locations
+        )
+        # Check results
+        self.assertTrue(arcpy.Exists(out_origins))
+        self.assertTrue(arcpy.Exists(out_destinations))
+        csv_files = glob(os.path.join(out_folder, "*.csv"))
+        self.assertGreater(len(csv_files), 0)
+
+    @unittest.skipIf(
+        helpers.arcgis_version < "2.9", "Arrow table output is not available in versions of Pro prior to 2.9.")
+    def test_run_tool_time_units_arrows(self):
+        """Test the tool with Apache Arrow outputs."""
+        # Run tool
+        out_origins = os.path.join(self.output_gdb, "Arrow_Origins")
+        out_destinations = os.path.join(self.output_gdb, "Arrow_Destinations")
+        out_folder = os.path.join(self.scratch_folder, "ArrowOutputs")
+        arcpy.LargeNetworkAnalysisTools.SolveLargeODCostMatrix(  # pylint: disable=no-member
+            self.origins,
+            self.destinations,
+            self.local_nd,
+            self.local_tm_dist,
+            "Minutes",
+            "Miles",
+            50,  # chunk size
+            4,  # max processes
+            out_origins,
+            out_destinations,
+            "Apache Arrow files",
+            None,
+            out_folder,
+            5,  # cutoff
+            2,  # number of destinations
+            None,  # barriers
+            True  # precalculate network locations
+        )
+        # Check results
+        self.assertTrue(arcpy.Exists(out_origins))
+        self.assertTrue(arcpy.Exists(out_destinations))
+        csv_files = glob(os.path.join(out_folder, "*.arrow"))
+        self.assertGreater(len(csv_files), 0)
+
+    def test_run_tool_service(self):
+        """Test that the tool runs with a service as a network data source."""
+        out_od_lines = os.path.join(self.output_gdb, "Service_ODLines")
+        out_origins = os.path.join(self.output_gdb, "Service_Origins")
+        out_destinations = os.path.join(self.output_gdb, "Service_Destinations")
+        arcpy.LargeNetworkAnalysisTools.SolveLargeODCostMatrix(  # pylint: disable=no-member
+            self.origins,
+            self.destinations,
+            self.portal_nd,
+            self.portal_tm,
+            "Minutes",
+            "Miles",
+            50,  # chunk size
+            4,  # max processes
+            out_origins,
+            out_destinations,
+            "Feature class",
+            out_od_lines,
+            None,
+            15,  # cutoff
+            1,  # number of destinations
             None,  # barriers
             True  # precalculate network locations
         )
@@ -112,7 +180,73 @@ class TestSolveLargeODCostMatrixTool(unittest.TestCase):
         self.assertTrue(arcpy.Exists(out_origins))
         self.assertTrue(arcpy.Exists(out_destinations))
 
-    def test_agol_max_processes(self):
+    def test_error_required_output_od_lines(self):
+        """Test for correct error when output format is Feature class and output OD Lines not specified."""
+        with self.assertRaises(arcpy.ExecuteError) as ex:
+            # Run tool
+            out_origins = os.path.join(self.output_gdb, "Err_Origins")
+            out_destinations = os.path.join(self.output_gdb, "Err_Destinations")
+            arcpy.LargeNetworkAnalysisTools.SolveLargeODCostMatrix(  # pylint: disable=no-member
+                self.origins,
+                self.destinations,
+                self.local_nd,
+                self.local_tm_time,
+                "Minutes",
+                "Miles",
+                50,  # chunk size
+                4,  # max processes
+                out_origins,
+                out_destinations,
+                "Feature class",
+                None,
+                "Junk",
+                15,  # cutoff
+                1,  # number of destinations
+                None,  # barriers
+                True  # precalculate network locations
+            )
+        expected_messages = [
+            "Failed to execute. Parameters are not valid.",
+            "ERROR 000735: Output OD Lines Feature Class: Value is required",
+            "Failed to execute (SolveLargeODCostMatrix)."
+        ]
+        actual_messages = str(ex.exception).strip().split("\n")
+        self.assertEqual(expected_messages, actual_messages)
+
+    def test_error_required_output_folder(self):
+        """Test for correct error when output format is Feature class and output OD Lines not specified."""
+        with self.assertRaises(arcpy.ExecuteError) as ex:
+            # Run tool
+            out_origins = os.path.join(self.output_gdb, "Err_Origins")
+            out_destinations = os.path.join(self.output_gdb, "Err_Destinations")
+            arcpy.LargeNetworkAnalysisTools.SolveLargeODCostMatrix(  # pylint: disable=no-member
+                self.origins,
+                self.destinations,
+                self.local_nd,
+                self.local_tm_time,
+                "Minutes",
+                "Miles",
+                50,  # chunk size
+                4,  # max processes
+                out_origins,
+                out_destinations,
+                "CSV files",
+                "Junk",
+                None,
+                15,  # cutoff
+                1,  # number of destinations
+                None,  # barriers
+                True  # precalculate network locations
+            )
+        expected_messages = [
+            "Failed to execute. Parameters are not valid.",
+            "ERROR 000735: Output Folder: Value is required",
+            "Failed to execute (SolveLargeODCostMatrix)."
+        ]
+        actual_messages = str(ex.exception).strip().split("\n")
+        self.assertEqual(expected_messages, actual_messages)
+
+    def test_error_agol_max_processes(self):
         """Test for correct error when max processes exceeds the limit for AGOL."""
         with self.assertRaises(arcpy.ExecuteError) as ex:
             # Run tool
@@ -122,15 +256,17 @@ class TestSolveLargeODCostMatrixTool(unittest.TestCase):
             arcpy.LargeNetworkAnalysisTools.SolveLargeODCostMatrix(  # pylint: disable=no-member
                 self.origins,
                 self.destinations,
-                out_od_lines,
-                out_origins,
-                out_destinations,
                 self.portal_nd,
                 self.portal_tm,
                 "Minutes",
                 "Miles",
                 50,  # chunk size
                 500,  # max processes
+                out_origins,
+                out_destinations,
+                "Feature class",
+                out_od_lines,
+                None,
                 15,  # cutoff
                 1,  # number of destinations
                 None,  # barriers
@@ -142,6 +278,42 @@ class TestSolveLargeODCostMatrixTool(unittest.TestCase):
                 f"The maximum number of parallel processes cannot exceed {helpers.MAX_AGOL_PROCESSES} when the "
                 "ArcGIS Online services are used as the network data source."
             ),
+            "Failed to execute (SolveLargeODCostMatrix)."
+        ]
+        actual_messages = str(ex.exception).strip().split("\n")
+        self.assertEqual(expected_messages, actual_messages)
+
+    @unittest.skipIf(
+        helpers.arcgis_version < "2.9", "Arrow table output is not available in versions of Pro prior to 2.9.")
+    def test_error_arrow_service(self):
+        """Test for correct error when the network data source is a service and requesting Arrow output."""
+        with self.assertRaises(arcpy.ExecuteError) as ex:
+            # Run tool
+            out_od_folder = os.path.join(self.scratch_folder, "Err")
+            out_origins = os.path.join(self.output_gdb, "Err_Origins")
+            out_destinations = os.path.join(self.output_gdb, "Err_Destinations")
+            arcpy.LargeNetworkAnalysisTools.SolveLargeODCostMatrix(  # pylint: disable=no-member
+                self.origins,
+                self.destinations,
+                self.portal_nd,
+                self.portal_tm,
+                "Minutes",
+                "Miles",
+                50,  # chunk size
+                4,  # max processes
+                out_origins,
+                out_destinations,
+                "Apache Arrow files",
+                None,
+                out_od_folder,
+                15,  # cutoff
+                1,  # number of destinations
+                None,  # barriers
+                True  # precalculate network locations
+            )
+        expected_messages = [
+            "Failed to execute. Parameters are not valid.",
+            "Apache Arrow files output format is not available when a service is used as the network data source.",
             "Failed to execute (SolveLargeODCostMatrix)."
         ]
         actual_messages = str(ex.exception).strip().split("\n")
